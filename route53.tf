514223b14d8a3d97023d6b62d41406b6916b59e7
Mostafa Khalil <mostafakhalil056@gmail.com>
2023-09-08 04:43:49 +0300

    route53

---
+++ route53.tf
@@ -0,0 +1,15 @@
+resource "aws_route53_zone" "primary" {
+  name = "aboutmostafa.com"
+}
+
+resource "aws_route53_record" "s3_bucket" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = ""  
+  type    = "A"
+
+  alias {
+    name                   = "http://aboutmostafa.com.s3-website.eu-central-1.amazonaws.com"
+    zone_id                = "Z21DNDUVLTQW6Q" 
+    evaluate_target_health = false
+  }
+}
